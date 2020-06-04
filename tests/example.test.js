70c40ab20882c24ce9f249f45be87533dd0029bd
Singh <singh2rajiv@dgb-c02vh08bh.bcg.com>
2020-06-03 18:26:21 -0700

    Added tests for feedback form

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,17 +1,21 @@
 import Homepage from "../pages/HomePage";
 import TopBar from "../pages/components/TopBar";
 import LoginPage from "../pages/LoginPage";
+import FeedbackPage from "../pages/FeedbackPage";
+
 describe('Example', () => {
 
     let homePage;
     let topBar;
     let loginPage;
+    let feedbackPage;
 
     beforeAll(async () => {
         jest.setTimeout(30000);
         homePage = new Homepage();  
         topBar = new TopBar();
         loginPage = new LoginPage();
+        feedbackPage = new FeedbackPage();
     })
     it('Homepage should work', async () => {
         await homePage.visit();
@@ -20,6 +24,17 @@ describe('Example', () => {
     it('Navbar should be displayed', async () => {
         await homePage.isNavbarDisplayed();
         await topBar.isTopBarDisplayed();
+    });
+
+    it('Feedback should work', async () => {
+        await feedbackPage.visit();
+        await feedbackPage.isFeedbackFormDisplayed();
+        await feedbackPage.submitFeedback(
+            "rajiv", 
+            "rajiv@rajiv.com", 
+            "testSubject",
+            "testComment"
+            );
     });
 
     it('Login with wrong credentials', async () => {
